2198999bb1a6e97d668d3e0e1d382e8fc0f64f46
phong <pppppppp1202@gmail.com>
2022-05-07 18:22:50 +0700

    add feature remove disc

--- Lab03/src/AimsProject/Aims.java
+++ Lab03/src/AimsProject/Aims.java
@@ -14,6 +14,12 @@ public class Aims {
 		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 		anOrder.addDigitalVideoDisc(dvd3);
+
+		
+		System.out.println(anOrder.removeDigitalVideoDisc(dvd3));
+		System.out.println(anOrder.removeDigitalVideoDisc(dvd3));
+		System.out.println(anOrder.removeDigitalVideoDisc(dvd3));
+		System.out.println(anOrder.removeDigitalVideoDisc(dvd3));
 		
 		System.out.print("The total cost is: ");
 		System.out.println(anOrder.totalCost());
